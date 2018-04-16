45ac812bd64dab23e7b77ffa5b1109b7e842b55c
Marcus Rodrigues <marcus_vpr@hotmail.com>
2018-04-16 13:51:42 -0300

    Projeto Criado

---
+++ src/main/java/com/mpxds/mprest/MprestApplication.java
@@ -0,0 +1,12 @@
+package com.mpxds.mprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mpxds/mprest/MprestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mpxds.mprest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MprestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
